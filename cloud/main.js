5dd63e0d98849c7a0f64058189d198b6cb5091f0
xxtonglei <xxtonglei@163.com>
2014-08-02 00:18:48 +0800

    modify main.js 222

--- cloud/main.js
+++ cloud/main.js
@@ -2,4 +2,48 @@
 // For example:
 AV.Cloud.define("hello", function(request, response) {
 	response.success("Hello world!");
-});
+})
+
+function pushMessage(bdUserId, messages, response){
+				//配置push对象
+			var opt = {
+				ak: '0C3jS31DYteNDW1HAM3TGcKV',
+				sk: '4mncUaMrC6L7h7Pqtf21XOx0azBGNcVa'
+			};
+			var client = new Push(opt);
+			//推送参数
+			var push_opt = {
+				push_type: 0,
+				user_id: bdUserId,
+				messages: JSON.stringify(messages),
+				msg_keys: JSON.stringify([new Date().getTime() + ""])
+			}
+			client.pushMsg(opt, function(err, result) {
+				if (err) {
+					console.log(err);
+					response.error(error);
+					return;
+				}
+				response.success(res);
+				console.log(result);
+			});
+}
+
+//发送消息函数
+AV.Cloud.define("sendMsg", function(request, response) {
+	var query = new AV.Query(AV.User);
+	query.equalTo("objectId", request.params.userId);
+	query.get(request.params.userId, {
+		success: function(user) {
+			var bdUserId = user.get("bd_userId");
+			var messages = request.params.messages;
+			pushMessage(bdUserId, messages, respones);
+		},
+		error: function(object, error) {
+			console.log(error);
+			response.error(error);
+		}
+	});
+})
+
+var Push = require('cloud/push');
