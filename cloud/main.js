c809db5c6dec0c53bf820a5f22c9b9c448f41c7c
xxtonglei <xxtonglei@163.com>
2014-08-02 15:05:02 +0800

    test function not ok601

--- cloud/main.js
+++ cloud/main.js
@@ -7,11 +7,46 @@ AV.Cloud.define("hello", function(request, response) {
 
 AV.Cloud.define("test", function(request, response) {
 
-var name = require('cloud/name.js');
+// var name = require('cloud/name.js');
 
-name.isACoolName('Fred', response);
+// name.isACoolName('Fred', response);
 // name.isACoolName('Skippy'), response); // 返回false
 	// response.success("test ok");
+
+
+	AV.Cloud.httpRequest({
+		method: 'POST',
+		url: "www.baidu.com",
+		headers: {
+			'Content-Length': bodyStr.length,
+			'Content-Type': 'application/x-www-form-urlencoded'
+		}
+		body: "q=fdfdfd",
+		success: function(httpResponse) {
+			var resBody = httpResponse.text;
+			// try {
+			// 	var jsonObj = JSON.parse(resBody);
+			// } catch (e) {
+			// 	cb && cb(e);
+			// 	return;
+			// }
+
+
+			response.success(httpResponse.text);
+
+			// var errObj = null;
+			// id.request_id = jsonObj['request_id'];
+			// parseRespone(id.request_id, httpResponse, cb);
+			// console.log(httpResponse.text);
+		},
+		error: function(httpResponse) {
+			response.error(httpResponse.status + "error");
+			// parseRespone(id.request_id, httpResponse, cb);
+			// console.error('Request failed with response code ' + httpResponse.status);
+		}
+	});
+
+	
 });
 
 
