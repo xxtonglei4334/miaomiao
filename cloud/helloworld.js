1e07c41d662e05f7d4cd076fa7c37a148ce82707
xxtonglei <xxtonglei@163.com>
2014-08-02 13:55:44 +0800

    test function not ok43

--- cloud/helloworld.js
+++ cloud/helloworld.js
@@ -1,3 +1,8 @@
-exports pritHelloWorld = function(){
+exports.pritHelloWorld = function(){
 	console.log("Hello World");
 }
+
+// var coolNames = ['Ralph', 'Skippy', 'Chip', 'Ned', 'Scooter'];
+// exports.isACoolName = function(name) {
+//   return coolNames.indexOf(name) !== -1;
+// }
