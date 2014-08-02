44ef92e420c04573bbde826aaa50b2c4758133bc
xxtonglei <xxtonglei@163.com>
2014-08-02 14:11:13 +0800

    test function not ok50

--- cloud/helloworld.js
+++ cloud/helloworld.js
@@ -1,3 +1,20 @@
+
+
+
+var util = require('util');
+// var assert = require('assert');
+var crypto = require('crypto');
+// var http = require('http');
+var querystring = require('querystring');
+var PROTOCOL_SCHEMA = 'http://';
+var SERVER_HOST = 'channel.api.duapp.com';
+var COMMON_PATH = '/rest/2.0/channel/';
+//var URL_HEADER = PROTOCOL_SCHEMA + SERVER_HOST;
+var debug = true;
+
+
+
+
 var coolNames = ['Ralph', 'Skippy', 'Chip', 'Ned', 'Scooter'];
 exports.isACoolName = function(name) {
   return coolNames.indexOf(name) !== -1;
